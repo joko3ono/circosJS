c58b6ed30d7e75c9d2f7b77803728c4638c774d6
Joko Triono <jokotriono369@gmail.com>
2024-08-13 10:08:38 +0700

    Update scale-chromatic version

--- src/behaviors/tooltip.js
+++ src/behaviors/tooltip.js
@@ -1,8 +1,6 @@
 import { select, pointer } from 'd3-selection'
 import 'd3-transition'
 
-import './tooltip.css'
-
 export function registerTooltip(track, instance, element, trackParams) {
   track.dispatch.on('mouseover', (event) => {
     const datum = d3.select(event.target).data()[0];
@@ -11,7 +9,15 @@ export function registerTooltip(track, instance, element, trackParams) {
       .transition()
       .style('opacity', 0.9)
       .style('left', (pointer(event, element)[0]) + 'px')
-      .style('top', (pointer(event, element)[1] - 28) + 'px');
+      .style('top', (pointer(event, element)[1] - 28) + 'px')
+      .style('position', 'absolute')
+      .style('text-align', 'center')
+      .style('padding', '5px 10px')
+      .style('background', '#111')
+      .style('color', 'white')
+      .style('border', '0px')
+      .style('pointer-events', 'none')
+      .style('z-index', 1000)
   });
 
   track.dispatch.on('mouseout', (event) => {
